645dfc40c1c8851bf572a253bc41f0cf4cfa038c
ionica21 <ionicat@windowslive.com>
2023-12-17 16:20:40 +0100

    test: add tests for day four

--- src/tasks/four.rs
+++ src/tasks/four.rs
@@ -1,7 +1,7 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Reindeer {
     name: String,
     strength: i32,
@@ -14,7 +14,7 @@ struct Reindeer {
     candies_eaten_yesterday: Option<i32>,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct ContestResponse {
     fastest: Option<String>,
     tallest: Option<String>,
@@ -90,4 +90,122 @@ async fn reindeer_contest(reindeers: web::Json<Vec<Reindeer>>) -> impl Responder
             None => None,
         },
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, App};
+    use serde_json;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_reindeer_strength() {
+        let app = test::init_service(App::new().service(reindeer_strength)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/4/strength")
+            .set_json(&vec![
+                Reindeer {
+                    name: "Dasher".to_string(),
+                    strength: 5,
+                    speed: None,
+                    height: None,
+                    antler_width: None,
+                    snow_magic_power: None,
+                    favorite_food: None,
+                    candies_eaten_yesterday: None,
+                },
+                Reindeer {
+                    name: "Dancer".to_string(),
+                    strength: 6,
+                    speed: None,
+                    height: None,
+                    antler_width: None,
+                    snow_magic_power: None,
+                    favorite_food: None,
+                    candies_eaten_yesterday: None,
+                },
+                Reindeer {
+                    name: "Prancer".to_string(),
+                    strength: 4,
+                    speed: None,
+                    height: None,
+                    antler_width: None,
+                    snow_magic_power: None,
+                    favorite_food: None,
+                    candies_eaten_yesterday: None,
+                },
+                Reindeer {
+                    name: "Vixen".to_string(),
+                    strength: 7,
+                    speed: None,
+                    height: None,
+                    antler_width: None,
+                    snow_magic_power: None,
+                    favorite_food: None,
+                    candies_eaten_yesterday: None,
+                },
+            ])
+            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+
+        let res_body = test::read_body(res).await;
+        assert_eq!(res_body, "22");
+    }
+
+    #[actix_web::test]
+    async fn test_reindeer_contest() {
+        let app = test::init_service(App::new().service(reindeer_contest)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/4/contest")
+            .set_json(&vec![
+                Reindeer {
+                    name: "Dasher".to_string(),
+                    strength: 5,
+                    speed: Some(50.4),
+                    height: Some(80),
+                    antler_width: Some(36),
+                    snow_magic_power: Some(9001),
+                    favorite_food: Some("hay".to_string()),
+                    candies_eaten_yesterday: Some(2),
+                },
+                Reindeer {
+                    name: "Dancer".to_string(),
+                    strength: 6,
+                    speed: Some(48.2),
+                    height: Some(65),
+                    antler_width: Some(37),
+                    snow_magic_power: Some(4004),
+                    favorite_food: Some("grass".to_string()),
+                    candies_eaten_yesterday: Some(5),
+                },
+            ])
+            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+
+        let res_body_bytes = test::read_body(res).await;
+        let res_body = String::from_utf8(res_body_bytes.to_vec())
+            .expect("Failed to convert response to string");
+
+        let content_response: ContestResponse =
+            serde_json::from_str(&res_body).expect("Failed to deserialize response");
+
+        assert_eq!(
+            content_response,
+            ContestResponse {
+                fastest: Some(
+                    "Speeding past the finish line with a strength of 5 is Dasher".to_string()
+                ),
+                tallest: Some("Dasher is standing tall with his 36 cm wide antlers".to_string()),
+                magician: Some(
+                    "Dasher could blast you away with a snow magic power of 9001".to_string()
+                ),
+                consumer: Some("Dancer ate lots of candies, but also some grass".to_string()),
+            }
+        );
+    }
 }
