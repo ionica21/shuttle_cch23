8a7f195d819adb7388efa93e5907495b1b98f17f
ionica21 <ionicat@windowslive.com>
2024-01-09 16:51:18 +0100

    feat: day 5 task 2

--- src/tasks/five.rs
+++ src/tasks/five.rs
@@ -3,8 +3,9 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct Paginate {
-    offset: usize,
-    limit: usize,
+    offset: Option<usize>,
+    limit: Option<usize>,
+    split: Option<usize>,
 }
 
 #[post("/5")]
@@ -12,11 +13,22 @@ async fn slice_names(
     paginate: web::Query<Paginate>,
     body: web::Json<Vec<String>>,
 ) -> impl Responder {
-    let start = paginate.offset;
-    let end = start + paginate.limit;
-    let sliced_names = &body[start..end.min(body.len())];
+    let offset = paginate.offset.unwrap_or(0);
+    let limit = paginate.limit.unwrap_or(body.len());
+    let split = paginate.split;
 
-    HttpResponse::Ok().json(sliced_names)
+    let end = (offset + limit).min(body.len());
+    let sliced = &body[offset..end];
+
+    match split {
+        Some(split_size) => HttpResponse::Ok().json(
+            sliced
+                .chunks(split_size)
+                .map(|chunk| chunk.to_vec())
+                .collect::<Vec<Vec<String>>>(),
+        ),
+        None => HttpResponse::Ok().json(sliced),
+    }
 }
 
 #[cfg(test)]
@@ -42,5 +54,49 @@ mod test {
         let body_str = String::from_utf8(body.to_vec()).expect("Failed to convert to UTF-8");
 
         assert_eq!(body_str, r#"["Owen","Lily","Ethan","Zoe","Nolan"]"#);
+    }
+
+    #[actix_web::test]
+    async fn test_slice_names_with_split() {
+        let mut app = test::init_service(App::new().service(slice_names)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/5?split=4")
+            .insert_header((header::CONTENT_TYPE, "application/json"))
+            .set_payload(r#"[ "Ava", "Caleb", "Mia", "Owen", "Lily", "Ethan", "Zoe", "Nolan", "Harper", "Lucas", "Stella", "Mason", "Olivia" ]"#)
+            .to_request();
+
+        let res = test::call_service(&mut app, req).await;
+        assert!(res.status().is_success());
+
+        let body = test::read_body(res).await;
+        let body_str = String::from_utf8(body.to_vec()).expect("Failed to convert to UTF-8");
+
+        assert_eq!(
+            body_str,
+            r#"[["Ava","Caleb","Mia","Owen"],["Lily","Ethan","Zoe","Nolan"],["Harper","Lucas","Stella","Mason"],["Olivia"]]"#
+        );
+    }
+
+    #[actix_web::test]
+    async fn test_slice_names_with_offset_and_split() {
+        let mut app = test::init_service(App::new().service(slice_names)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/5?offset=5&split=2")
+            .insert_header((header::CONTENT_TYPE, "application/json"))
+            .set_payload(r#"[ "Ava", "Caleb", "Mia", "Owen", "Lily", "Ethan", "Zoe", "Nolan", "Harper", "Lucas", "Stella", "Mason", "Olivia" ]"#)
+            .to_request();
+
+        let res = test::call_service(&mut app, req).await;
+        assert!(res.status().is_success());
+
+        let body = test::read_body(res).await;
+        let body_str = String::from_utf8(body.to_vec()).expect("Failed to convert to UTF-8");
+
+        assert_eq!(
+            body_str,
+            r#"[["Ethan","Zoe"],["Nolan","Harper"],["Lucas","Stella"],["Mason","Olivia"]]"#
+        );
     }
 }
